2162bd500086178ad2a048b8fe75c42fdd6c8074
leopardracer <136604165+leopardracer@users.noreply.github.com>
2025-05-17 19:01:41 +0300

    Update index.ts

--- fees/bonzo/index.ts
+++ fees/bonzo/index.ts
@@ -8,7 +8,7 @@ const fetch = async () => {
     const total_protocol_fees = new BigNumber(res.total_protocol_fees.usd_wad);
     // Total of earned intrest for liquidity providers over the 24hr period.
     const total_intrest_earned = new BigNumber(res.total_intrest_earned.usd_wad);
-    // Total of just the premium paid back when flash loan borrowing, does not included ammount borrowed
+    // Total of just the premium paid back when flash loan borrowing, does not included amount borrowed
     const total_flash_loan_fees = new BigNumber(res.total_flash_loan_fees.usd_wad);
     // Total of just the bonus portion given to liquidators
     const total_liquidation_bonuses = new BigNumber(res.total_liquidation_bonuses.usd_wad);
@@ -20,7 +20,7 @@ const fetch = async () => {
         total_gas_consumed (number)
         total_gas_charged (number)
         total_network_fees (total value of hedera network fees, excluding gas)
-        total_gas_fees (total value of gas fees paid to the hedera netwrok)
+        total_gas_fees (total value of gas fees paid to the hedera network)
     */
     const dailyFees = total_protocol_fees.plus(total_intrest_earned).plus(total_flash_loan_fees).plus(total_liquidation_bonuses).shiftedBy(-18);
     const dailyUserFees = dailyFees;
